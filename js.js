e2a5d7fcc20b0c996a2a57c1a3a6699f871a9f68
Abdul Wahab Raza <abdulwahabraza00000@gmail.com>
2022-04-25 19:35:47 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,6 @@
+function Plus()
+{
+    let screen=document.parseInt(getElementById('screen').value);
+    let screenValue=document.parseInt(getElementById('buttonPlus').value);
+   screen+=screenValue;
+}
